485653a7a38569249f1798cccea5d3e83a9b69db
Kornel Ługowski <k.lugowski77@gmail.com>
2023-04-11 08:33:17 +0200

    Started JWT

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
+const {verifyToken, verifyTokenAndAuthorization} = require("./verifyToken")
 
-
+router.put("/:id", verifyTokenAndAuthorization, (req, res)=>{
+    if(req.body.password){
+            
+        req.body.password = hashedPassword
+    }
+})
 
 module.exports = router;
